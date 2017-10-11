7e07b94bc835f838f5fba78e9d663c6829d5347c
Kyle Stone <kylestone87@gmail.com>
2017-10-11 16:49:29 -0700

    removed border styling for incorrect input

--- website/js/script.js
+++ website/js/script.js
@@ -8,8 +8,10 @@ function myFunction () {
             }
             star += "<br \>";//after all columns, insert carriage return to start a new line
         }
+        document.getElementById("star").style.border = "solid green";
         document.getElementById("star").innerHTML = star;//display the stars in element ID "star"
         var y = Math.log2(x/0.000001); //the amount of 2's to divide by to get less than 0.000001
+        document.getElementById("response").style.color = "black";
         document.getElementById("response").innerHTML = 
         "Your input number is "+ x + ". <br \> <br />The number of times to divide the number of "+ x +" by 2 to get a value of less than one millionth is " + y.toFixed(0) + ". <br />"
     // displays y, shortened to remove decimals. Value is rounded up to nearest whole number.
@@ -17,7 +19,7 @@ function myFunction () {
     else {  //user didn't enter a correct value
         document.getElementById("response").innerHTML = 
         "Your input is " + x + ". A valid number is between 2 and 10. Please reload the page and try again.";
-        document.getElementById("star").removeAttribute("borde-style");
+        document.getElementById("star").style.border = "none";
     }
     
 }
